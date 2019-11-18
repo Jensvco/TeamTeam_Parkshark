330bd57431d2d1d2d528e9214163a558826ae030
jensvco <jens.vancoppernolle@hotmail.com>
2019-11-18 09:57:40 +0100

    initial commit

---
+++ src/test/java/com/switchfully/teamteam/parkshark/ParksharkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.switchfully.teamteam.parkshark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParksharkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
