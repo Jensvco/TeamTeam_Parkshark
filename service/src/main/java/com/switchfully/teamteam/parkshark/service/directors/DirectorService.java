cda013b4eebf871dab3632e733c8bba9c7ed2e12
Thomas Dardenne <dardenne.thomas@gmail.com>
2019-11-18 15:27:20 +0100

    Adding Transactional annotation

--- service/src/main/java/com/switchfully/teamteam/parkshark/service/directors/DirectorService.java
+++ service/src/main/java/com/switchfully/teamteam/parkshark/service/directors/DirectorService.java
@@ -4,8 +4,10 @@ import com.switchfully.teamteam.parkshark.domain.directors.Director;
 import com.switchfully.teamteam.parkshark.domain.directors.DirectorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class DirectorService {
 
     private final DirectorRepository directorRepository;
--- service/src/main/java/com/switchfully/teamteam/parkshark/service/divisions/DivisionService.java
+++ service/src/main/java/com/switchfully/teamteam/parkshark/service/divisions/DivisionService.java
@@ -4,10 +4,12 @@ import com.switchfully.teamteam.parkshark.domain.divisions.Division;
 import com.switchfully.teamteam.parkshark.domain.divisions.DivisionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class DivisionService {
 
     private final DivisionRepository divisionRepository;
