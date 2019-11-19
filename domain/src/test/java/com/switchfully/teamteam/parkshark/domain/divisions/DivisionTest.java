a770a2e4d3cf7faa8b304ecaed4b0d75f2b54e46
Thomas Dardenne <dardenne.thomas@gmail.com>
2019-11-19 10:09:53 +0100

    Updating the domain to add subdivisions

--- domain/src/test/java/com/switchfully/teamteam/parkshark/domain/divisions/DivisionTest.java
+++ domain/src/test/java/com/switchfully/teamteam/parkshark/domain/divisions/DivisionTest.java
@@ -1,5 +1,7 @@
 package com.switchfully.teamteam.parkshark.domain.divisions;
 
+import com.switchfully.teamteam.parkshark.domain.directors.Director;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.switchfully.teamteam.parkshark.domain.directors.Director.DirectorBuilder.director;
@@ -8,14 +10,19 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class DivisionTest {
 
-    @Test
-    void createDivision() {
-        // given & when
-        var director = director()
+    private Director director;
+
+    @BeforeEach
+    void setUp() {
+        director = director()
                 .withFirstName("Nick")
                 .withLastName("Meert")
                 .build();
+    }
 
+    @Test
+    void createDivision_withoutParent() {
+        // given & when
         var division = division()
                 .withDirector(director)
                 .withName("IT")
@@ -26,6 +33,31 @@ class DivisionTest {
         assertThat(division.getDirector()).isEqualToComparingFieldByField(director);
         assertThat(division.getName()).isEqualTo("IT");
         assertThat(division.getOriginalName()).isEqualTo("Technical guys");
+        assertThat(division.getParent()).isNull();
+    }
+
+    @Test
+    void createDivision_withParent() {
+        // given & when
+        var parent = division()
+                .withDirector(director)
+                .withName("Big Boss")
+                .withOriginalName("Bossy Bossy")
+                .build();
+
+        var division = division()
+                .withDirector(director)
+                .withName("IT")
+                .withOriginalName("Technical")
+                .withParent(parent)
+                .build();
+
+        assertThat(parent.getParent()).isNull();
+        assertThat(division.getDirector()).isEqualToComparingFieldByField(director);
+        assertThat(division.getName()).isEqualTo("IT");
+        assertThat(division.getOriginalName()).isEqualTo("Technical");
+        assertThat(division.getParent()).usingRecursiveComparison()
+                .isEqualTo(parent);
     }
 
 }
