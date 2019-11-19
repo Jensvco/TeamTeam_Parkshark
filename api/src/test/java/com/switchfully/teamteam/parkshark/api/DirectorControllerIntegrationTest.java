02b4002eda898b77bb5531112181a691f862a4bb
thomas <thomas.dardenne@student.unamur.be>
2019-11-19 08:09:22 +0100

    Setting up integration tests

--- api/src/test/java/com/switchfully/teamteam/parkshark/api/DirectorControllerIntegrationTest.java
+++ api/src/test/java/com/switchfully/teamteam/parkshark/api/DirectorControllerIntegrationTest.java
@@ -1,4 +1,17 @@
 package com.switchfully.teamteam.parkshark.api;
 
-public class DirectorControllerIntegrationTest {
+import com.switchfully.teamteam.parkshark.ControllerIntegrationTest;
+import com.switchfully.teamteam.parkshark.domain.directors.DirectorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class DirectorControllerIntegrationTest extends ControllerIntegrationTest {
+
+    @Autowired
+    private DirectorRepository directorRepository;
+
+    @Override
+    public void clearDatabase() {
+
+    }
+
 }
