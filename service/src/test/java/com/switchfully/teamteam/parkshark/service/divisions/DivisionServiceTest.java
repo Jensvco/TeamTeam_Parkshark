465495daa11214ae2ab72f96855d5c71c2f4d65d
thomas <thomas.dardenne@student.unamur.be>
2019-11-19 07:42:26 +0100

    Adding tests for getAllDivisions

--- service/src/test/java/com/switchfully/teamteam/parkshark/service/divisions/DivisionServiceTest.java
+++ service/src/test/java/com/switchfully/teamteam/parkshark/service/divisions/DivisionServiceTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
+import java.util.List;
+
 import static com.switchfully.teamteam.parkshark.domain.directors.Director.DirectorBuilder.director;
 import static com.switchfully.teamteam.parkshark.domain.divisions.Division.DivisionBuilder.division;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -31,7 +33,7 @@ class DivisionServiceTest {
     void setUp() {
         initMocks(this);
 
-        division()
+        division = division()
                 .withDirector(director()
                         .withFirstName("Nick")
                         .withLastName("Meert")
@@ -58,6 +60,29 @@ class DivisionServiceTest {
 
         assertThatThrownBy(() -> divisionService.createDivision(division))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void getAllDivisions_hasRightSize() {
+        when(divisionRepository.findAll()).thenReturn(List.of(division));
+
+        assertThat(divisionService.getAllDivisions()).hasSize(1);
+    }
+
+    @Test
+    void getAllDivisions_hasRightContent() {
+        when(divisionRepository.findAll()).thenReturn(List.of(division));
+
+        assertThat(divisionService.getAllDivisions().get(0))
+                .usingRecursiveComparison()
+                .isEqualTo(division()
+                        .withDirector(director()
+                                .withFirstName("Nick")
+                                .withLastName("Meert")
+                                .build())
+                        .withName("IT")
+                        .withOriginalName("Technical guys")
+                        .build());
     }
 
 }
