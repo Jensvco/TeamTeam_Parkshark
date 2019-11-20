07514869424c367d31948349bbd6cbee3bf24f77
jensvco <jens.vancoppernolle@hotmail.com>
2019-11-20 14:39:39 +0100

    Created a getAllParkingLots IntegrationTest

--- api/src/test/java/com/switchfully/teamteam/parkshark/api/parking_lots/ParkingLotControllerIntegrationTest.java
+++ api/src/test/java/com/switchfully/teamteam/parkshark/api/parking_lots/ParkingLotControllerIntegrationTest.java
@@ -5,7 +5,10 @@ import com.switchfully.teamteam.parkshark.api.addresses.dto.CreateAddressDto;
 import com.switchfully.teamteam.parkshark.api.contact_persons.dto.CreateContactPersonDto;
 import com.switchfully.teamteam.parkshark.api.phone_numbers.CreatePhoneNumberDto;
 import com.switchfully.teamteam.parkshark.domain.PhoneNumber;
+import com.switchfully.teamteam.parkshark.domain.repositories.ParkingLotRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
 import java.util.ArrayList;
@@ -18,9 +21,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ParkingLotControllerIntegrationTest extends ControllerIntegrationTest {
     public static final String LOCALHOST = "http://localhost";
 
+    @Autowired
+    private ParkingLotRepository parkingLotRepository;
+
     @Override
     public void clearDatabase() {
+        parkingLotRepository.deleteAll();
+    }
 
+    @BeforeEach
+    void setup() {
+        clearDatabase();
     }
 
     @Test
@@ -37,6 +48,29 @@ public class ParkingLotControllerIntegrationTest extends ControllerIntegrationTe
                 .isEqualTo(createAParkingLot());
     }
 
+
+    @Test
+    void getAllParkingLots() {
+        new TestRestTemplate()
+                .postForObject(format(LOCALHOST + ":%s/parkinglots",
+                        getPort()),
+                        createAParkingLot(),
+                        CreateParkingLotDto.class);
+
+        new TestRestTemplate()
+                .postForObject(format(LOCALHOST + ":%s/parkinglots",
+                        getPort()),
+                        createAParkingLot(),
+                        CreateParkingLotDto.class);
+
+        ParkingLotDto[] allParkingLots = new TestRestTemplate()
+                .getForObject(format(LOCALHOST + ":%s/parkinglots",
+                                getPort()),
+                                ParkingLotDto[].class);
+
+        assertThat(allParkingLots).hasSize(2);
+    }
+
     private CreateParkingLotDto createAParkingLot() {
         List<CreatePhoneNumberDto> phoneNumberList = new ArrayList<>();
         CreatePhoneNumberDto phoneNumber = new CreatePhoneNumberDto("0474531267", PhoneNumber.PhoneNumberType.MOBILE_PHONE);
@@ -50,7 +84,5 @@ public class ParkingLotControllerIntegrationTest extends ControllerIntegrationTe
                 .withContactPerson(new CreateContactPersonDto("Jens", phoneNumberList, "Jensvco@gmail.com", new CreateAddressDto("Street", "Number", "9000", "Gent")))
                 .build();
     }
-
-
 
 }
