d6430615de040776ba2309e8d3a455087b10ae22
Thomas Dardenne <dardenne.thomas@gmail.com>
2019-11-20 15:16:52 +0100

    Adding a starting date to Membership

--- domain/src/main/java/com/switchfully/teamteam/parkshark/domain/memberships/Membership.java
+++ domain/src/main/java/com/switchfully/teamteam/parkshark/domain/memberships/Membership.java
@@ -1,15 +1,19 @@
 package com.switchfully.teamteam.parkshark.domain.memberships;
 
+import java.time.LocalDate;
+
 public abstract class Membership {
 
     private double monthlyCost;
     private double allocationReduction;
     private int maximumAllowedAllocationHours;
+    private LocalDate startingDate;
 
     public Membership(double monthlyCost, double allocationReduction, int maximumAllowedAllocationHours) {
         this.monthlyCost = monthlyCost;
         this.allocationReduction = allocationReduction;
         this.maximumAllowedAllocationHours = maximumAllowedAllocationHours;
+        this.startingDate = LocalDate.now();
     }
 
     public double getMonthlyCost() {
@@ -22,6 +26,10 @@ public abstract class Membership {
 
     public int getMaximumAllowedAllocationHours() {
         return maximumAllowedAllocationHours;
+    }
+
+    public LocalDate getStartingDate() {
+        return startingDate;
     }
 
 }
--- domain/src/test/java/com/switchfully/teamteam/parkshark/domain/memberships/MembershipTest.java
+++ domain/src/test/java/com/switchfully/teamteam/parkshark/domain/memberships/MembershipTest.java
@@ -1,7 +1,18 @@
 package com.switchfully.teamteam.parkshark.domain.memberships;
 
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class MembershipTest {
 
+    @Test
+    void createMembership_thenMembershipShouldHaveTodayAsStartingDate() {
+        var membership = new BronzeMembership();
 
+        assertThat(membership.getStartingDate()).isEqualTo(LocalDate.now());
+    }
 
 }
